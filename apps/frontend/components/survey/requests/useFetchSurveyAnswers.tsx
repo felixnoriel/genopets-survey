f2d2bb5081a129b7ba264a822d6a174c97318e2f
felixnoriel <jrnoriel_56@yahoo.com>
2022-03-07 19:20:38 +1100

    add states to check whether user has already submitted an answer on page load

--- apps/frontend/components/survey/requests/useFetchSurveyAnswers.tsx
+++ apps/frontend/components/survey/requests/useFetchSurveyAnswers.tsx
@@ -1,19 +1,36 @@
 import { useSurveyAnswerStore } from '../../../lib/stores/survey';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import { axiosInstance } from '../../../lib/http-requests/axios';
 
-export const useFetchSurveyAnswers = () => {
-  const { setAnswers } = useSurveyAnswerStore();
+export const useFetchSurveyAnswers = (): {
+  isLoading: boolean;
+} => {
+  const { setAnswers, setAnswerExists } = useSurveyAnswerStore();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchSurveyAnswers = async () => {
       const { data } = await axiosInstance.get('/survey/answers');
       const { data: surveyAnswers } = data;
 
-      setAnswers(surveyAnswers);
+      const answers = {} as Record<string, string[]>;
+      for (const answer of surveyAnswers) {
+        answers[answer.question_id] = answer.answer;
+      }
+
+      setAnswers(answers);
+
+      if (surveyAnswers.length > 0) {
+        setAnswerExists(true);
+      }
+      setIsLoading(false);
     };
 
     fetchSurveyAnswers();
   }, []);
+
+  return {
+    isLoading,
+  };
 };
--- apps/frontend/lib/stores/survey.ts
+++ apps/frontend/lib/stores/survey.ts
@@ -1,15 +1,25 @@
 import create from 'zustand';
 
-type SurveyAnswerType = {
+type SurveyAnswerStoreType = {
   answers: Record<string, string[]>;
   setAnswers: (answers: Record<string, string[]>) => void;
+  answerExists: boolean;
+  setAnswerExists: (value: boolean) => void;
 };
 
-export const useSurveyAnswerStore = create<SurveyAnswerType>((set, get) => ({
-  answers: {},
-  setAnswers: (answers: Record<string, string[]>) => {
-    return set({
-      answers,
-    });
-  },
-}));
+export const useSurveyAnswerStore = create<SurveyAnswerStoreType>(
+  (set, get) => ({
+    answers: {},
+    setAnswers: (answers: Record<string, string[]>) => {
+      return set({
+        answers,
+      });
+    },
+    answerExists: false,
+    setAnswerExists: (value: boolean) => {
+      return set({
+        answerExists: value,
+      });
+    },
+  }),
+);
